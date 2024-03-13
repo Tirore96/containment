486e03e43f31f5116b36fea359841032f766a45e
Tirore96 <dawit.tirore@gmail.com>
2024-03-13 20:45:41 +0000

    soundness and interpretation section is decent now

---
+++ bench/bin/main.ml
@@ -0,0 +1,34 @@
+open Bench.Testsuite
+
+let () = 
+         let _ = test1 () in
+         let _ = test1' () in
+
+         let _ = test2 () in
+         let _ = test2' () in
+
+         let _ = test3 () in
+         let _ = test3' () in
+
+         let _ = test4 () in
+         let _ = test4' () in
+
+         let _ = test5 () in
+         let _ = test5' () in
+
+         let _ = test6 () in
+         let _ = test6' () in
+
+         let _ = test7 () in
+         let _ = test7' () in
+
+         let _ = test8 () in
+         let _ = test8' () in
+
+         let _ = test9 () in
+         let _ = test9' () in
+
+         let _ = test10 () in
+         let _ = test10' () in
+         ()
+
